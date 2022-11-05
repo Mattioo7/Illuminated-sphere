4d4dde995fa72211c187014790319fb449603fa4
Mateusz Chudek <mati.chudek@op.pl>
2022-11-05 18:08:24 +0100

    Test load of obj fine

--- Illuminated sphere/Form1.Designer.cs
+++ Illuminated sphere/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Illuminated_sphere;
 
-partial class Form1
+partial class form_mainWindow
 {
     /// <summary>
     ///  Required designer variable.
@@ -29,18 +29,51 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
+			this.tableLayoutPanel_main = new System.Windows.Forms.TableLayoutPanel();
+			this.pictureBox_workingArea = new System.Windows.Forms.PictureBox();
+			this.tableLayoutPanel_main.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_workingArea)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// Form1
+			// tableLayoutPanel_main
+			// 
+			this.tableLayoutPanel_main.ColumnCount = 2;
+			this.tableLayoutPanel_main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel_main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+			this.tableLayoutPanel_main.Controls.Add(this.pictureBox_workingArea, 0, 0);
+			this.tableLayoutPanel_main.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel_main.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel_main.Name = "tableLayoutPanel_main";
+			this.tableLayoutPanel_main.RowCount = 1;
+			this.tableLayoutPanel_main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel_main.Size = new System.Drawing.Size(984, 761);
+			this.tableLayoutPanel_main.TabIndex = 0;
+			// 
+			// pictureBox_workingArea
+			// 
+			this.pictureBox_workingArea.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox_workingArea.Location = new System.Drawing.Point(3, 3);
+			this.pictureBox_workingArea.Name = "pictureBox_workingArea";
+			this.pictureBox_workingArea.Size = new System.Drawing.Size(778, 755);
+			this.pictureBox_workingArea.TabIndex = 0;
+			this.pictureBox_workingArea.TabStop = false;
+			// 
+			// form_mainWindow
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(884, 561);
-			this.Name = "Form1";
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(984, 761);
+			this.Controls.Add(this.tableLayoutPanel_main);
+			this.Name = "form_mainWindow";
+			this.Text = "Illuminated sphere";
+			this.tableLayoutPanel_main.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_workingArea)).EndInit();
 			this.ResumeLayout(false);
 
     }
 
     #endregion
+
+    private TableLayoutPanel tableLayoutPanel_main;
+    private PictureBox pictureBox_workingArea;
 }
--- Illuminated sphere/Form1.cs
+++ Illuminated sphere/Form1.cs
@@ -1,9 +1,28 @@
+using ObjLoader.Loader.Loaders;
+
 namespace Illuminated_sphere;
 
-public partial class Form1 : Form
+public partial class form_mainWindow : Form
 {
-    public Form1()
-    {
-        InitializeComponent();
-    }
+	public form_mainWindow()
+	{
+		InitializeComponent();
+
+		obj_test();
+	}
+
+	private void obj_test()
+	{
+		LoadResult loadResult = new LoadResult();
+
+		var objLoaderFactory = new ObjLoaderFactory();
+		var objLoader = objLoaderFactory.Create();
+
+		string fileName = "sphere.obj";
+		string path = Path.Combine(Environment.CurrentDirectory, @"Props\", fileName);
+
+		var fileStream = new FileStream(path, FileMode.Open);
+		var result = objLoader.Load(fileStream);
+
+	}
 }
